91caaa014db1ba236df7a32bd6491daeb90ab41b
Tom Conchie <tconchie@gmail.com>
2017-07-19 22:27:54 -0600

    Adds stylesheets and favicon

--- server/app.js
+++ server/app.js
@@ -3,11 +3,15 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+
+
 
 var app = express();
 
 var users = require('./routes/users');
 
+app.use(favicon(__dirname + '/../client/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
